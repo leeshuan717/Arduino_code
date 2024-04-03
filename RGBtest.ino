ecbc69bbbcad3174941bb9eda19ff38f3ae20583
maqiduo <128950209+leeshuan717@users.noreply.github.com>
2024-04-04 00:39:12 +0800

    Create RGBtest.ino

---
+++ RGBtest.ino
@@ -0,0 +1,23 @@
+#include <DFRobot_RGBMatrix.h>
+
+#define OE   	9
+#define LAT 	10
+#define CLK 	11
+#define A   	A0
+#define B   	A1
+#define C   	A2
+#define D   	A3
+#define E   	A4
+#define WIDTH 64
+#define _HIGH	64 
+
+DFRobot_RGBMatrix matrix(A, B, C, D, E, CLK, LAT, OE, false, WIDTH, _HIGH);
+
+void setup() {
+  matrix.begin();
+  matrix.fillScreen(matrix.Color333(0, 0, 7));//red , green , blue
+}
+
+
+void loop() {
+}
